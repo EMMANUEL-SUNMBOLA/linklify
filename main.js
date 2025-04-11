0b234ddb027297531be198148ae290f1d4b75e20
Emmanuel-Sunmbola <adedayoemmanuel729@gmail.com>
2025-04-11 20:23:53 +0100

    first fetch from our API

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ let logo = document.querySelector(".brand")
 logo.addEventListener('click', ()=>{
     window.location.href = '/index.html'
 })
+
+fetch('http://localhost:5543/verify', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: "Emmanuel77%",
+      email: "adedayoforgit@g9mail.com",
+      password: "20052907"
+    })
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
